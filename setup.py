9e73ef6d598a498350ea5eef54dd0982830ae014
Christian Boelsen <christian.boelsen@hds.com>
2016-10-18 15:59:34 +0100

    Sort out setup metadata for pypi.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.0.1',
     author='Christian Boelsen',
     author_email='christianboelsen+github@gmail.com',
-    packages = find_packages(exclude=["tests"]),
-    entry_points = {
+    packages=find_packages(exclude=["tests"]),
+    entry_points={
         'console_scripts': [
             'apium-worker = apium.command:start_workers',
             'apium-inspect = apium.command:inspect',
@@ -16,4 +16,14 @@ setup(
     license='LICENSE',
     description=open('README').read(),
     install_requires=[],
+    keywords=['celery', 'task', 'queue'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
