9af3fb758874b8ef8201e6eaff900bbe3d37b75a
Christian Boelsen <christian.boelsen@hds.com>
2016-12-05 11:40:23 +0000

    First (broken) attempt at using pbr.

--- setup.py
+++ setup.py
@@ -1,29 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name='apium',
-    version='0.0.2',
-    author='Christian Boelsen',
-    author_email='christianboelsen+github@gmail.com',
-    packages=find_packages(exclude=["tests"]),
     entry_points={
         'console_scripts': [
             'apium-worker = apium.command:start_workers',
             'apium-inspect = apium.command:inspect',
         ],
     },
-    license='LICENSE',
-    description=open('README').read(),
-    install_requires=[],
-    keywords=['celery', 'task', 'queue'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-    ],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
