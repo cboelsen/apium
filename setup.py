4dcf7848a6206d1c26ce933b3d809e53d9f80d22
Christian Boelsen <christian.boelsen@hds.com>
2016-12-06 14:53:42 +0000

    Use pbr fully; automate deployment with travis-ci.

--- setup.py
+++ setup.py
@@ -1,24 +1,7 @@
-import sys
-
 from setuptools import setup
 
 
-requirements = []
-if sys.version_info.major < 3:
-    requirements += [
-        'futures',
-        'funcsigs',
-    ]
-
-
 setup(
-    entry_points={
-        'console_scripts': [
-            'apium-worker = apium.command:start_workers',
-            'apium-inspect = apium.command:inspect',
-        ],
-    },
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    install_requires=requirements,
     pbr=True,
 )
